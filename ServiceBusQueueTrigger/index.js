699802a1567603dd6b7f0277eabf95ec76846b35
lukas braun <lukas.braun@mailbox.org>
2021-01-22 23:36:56 +0100

    Add JSON.stringify to logs

--- ServiceBusQueueTrigger/index.js
+++ ServiceBusQueueTrigger/index.js
@@ -4,10 +4,10 @@ module.exports = async function(context, mySbMsg) {
     const parse = (msg) => {
         if(!msg) return {};
         const parsed = documentParser().val(msg.text);
-        context.log(`text: ${parsed}`);
+        context.log(`text: ${JSON.stringify(parsed)}`);
         return parsed;
     }
-    context.log('ServiceBus queue trigger function processed message', mySbMsg);
+    context.log('ServiceBus queue trigger function processed message', JSON.stringify(mySbMsg));
     const res = parse(mySbMsg);
     const outputSbMsg = {...res, fileName: mySbMsg.fileName}
     context.bindings.outputSbMsg = JSON.stringify(outputSbMsg);
