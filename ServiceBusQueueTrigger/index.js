a8890a89940ca780abb1863a14b15fb48db01855
lukas braun <lukas.braun@mailbox.org>
2021-01-22 21:18:39 +0100

    Convert to JS

---
+++ ServiceBusQueueTrigger/index.js
@@ -0,0 +1,13 @@
+module.exports = async function(context, mySbMsg) {
+    const parse = (msg) => {
+        if(!msg) return {};
+        const parsed = documentParser().val(msg.text);
+        console.log(`text: ${parsed}`);
+        return parsed;
+    }
+    context.log('ServiceBus queue trigger function processed message', mySbMsg);
+    const res = parse(mySbMsg);
+    const outputSbMsg = {...res, fileName: mySbMsg.fileName}
+    context.bindings.outputSbMsg = JSON.stringify(outputSbMsg);
+    context.done();
+};
---
+++ ServiceBusQueueTrigger/parsing.js
@@ -0,0 +1,41 @@
+const { C } = require('@masala/parser');
+
+const whitespaceParser = () => C.char(' ').optrep();
+
+const untilLineBreakParser = () => C.notString('\n').rep()
+
+const title = () => C.stringLiteral().or(untilLineBreakParser());
+
+const titleParser = () => C.string('#+title:').then(whitespaceParser()).drop().then(title()).map(x => x.value.reduce((acc, res) => ({
+    ...acc,
+    value: [acc.value + res]
+}), { value: [], type: 'title' }));
+
+const tagOrAlias = () => whitespaceParser().drop().then(C.stringLiteral().or(C.letters()))
+
+const tagAliasCollector = (name) => tagOrAlias().rep().map(x => x.value.reduce((acc, res) => ({
+    ...acc,
+    value: [...acc.value, res]
+}), { value: [], type: name }));
+
+const tagAliasParser = (name) => C.string(`#+${name}:`).then(whitespaceParser()).drop().then(tagAliasCollector(name))
+
+const tagOrAliasParser = () => tagAliasParser('alias').or(tagAliasParser('tags')).then(whitespaceParser().drop()).then(C.char('\n').opt().drop())
+
+const stringInReferenceParser = () => C.notChar(']').rep().map(x => x.value)
+
+const endParser = () => C.char(']').or(C.char('[')).drop();
+
+const reference = () => C.string('[[')
+    .then(C.string("file:"))
+    .drop()
+    .then(stringInReferenceParser())
+    .then(endParser().rep())
+    .then(stringInReferenceParser())
+    .then(endParser().rep())
+    .map(x => x.value.map(y => y.reduce((acc, res) => ({ ...acc, value: [acc.value + res] })
+        , { type: "reference", value: [] }))[0])
+
+const textParser = () => reference().or((C.letters()).or(C.char(' ').or(C.notChar(' '))).drop()).rep()
+
+module.exports = () => titleParser().then(C.char('\n').drop()).then(tagOrAliasParser().optrep()).then(textParser())
