e03cea415223caa0e326525696933449aef6301d
lukas braun <lukas.braun@mailbox.org>
2021-01-22 23:24:11 +0100

    Add log

--- ServiceBusQueueTrigger/index.js
+++ ServiceBusQueueTrigger/index.js
@@ -2,7 +2,7 @@ module.exports = async function(context, mySbMsg) {
     const parse = (msg) => {
         if(!msg) return {};
         const parsed = documentParser().val(msg.text);
-        console.log(`text: ${parsed}`);
+        context.log(`text: ${parsed}`);
         return parsed;
     }
     context.log('ServiceBus queue trigger function processed message', mySbMsg);
